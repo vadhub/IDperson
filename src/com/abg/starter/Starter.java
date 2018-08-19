06ed2065ec330dfb039f68855bc25d6ff716f18a
Vadim <gabderahmanov99@gmail.com>
2018-08-19 11:18:35 +0300

    add gui

--- src/com/abg/starter/Starter.java
+++ src/com/abg/starter/Starter.java
@@ -1,10 +1,47 @@
 package com.abg.starter;
 
-import com.abg.person.ID;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Starter {
 	public static void main(String[] args) {
-		ID id = new ID();
-		System.out.println(id.idPerson());
+		JFrame frame = new JFrame("AddPerson");
+		JPanel panelMain = new JPanel(new GridLayout(6,10));
+		
+		JTextField name = new JTextField();
+		JTextField pass = new JTextField();
+		
+		name.setText("Name");
+		pass.setText("Pass");
+		
+		name.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				name.setText("");				
+			}
+		});
+		
+		pass.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				pass.setText("");
+			}
+		});
+		JButton add = new JButton("Add");
+		
+		panelMain.add(name);
+		panelMain.add(pass);
+		panelMain.add(add);
+		
+		frame.setSize(300, 200);
+		frame.add(panelMain);
+		frame.setLocation(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
 	}
 }
